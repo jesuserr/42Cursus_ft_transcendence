7f484cc3d9d6d501068c9acfed3caba102a0196e
cescanue <cescanuela@gmail.com>
2024-04-16 12:28:09 +0200

    postgres

--- infra/scrs/requirements/postgresql/conf/create.sh
+++ infra/scrs/requirements/postgresql/conf/create.sh
@@ -3,3 +3,4 @@ sed "s/DB_PASS/$DB_PASS/g" /tmp/create.sql > tmp1; cat tmp1 > /tmp/create.sql; r
 sed "s/DB_DB/$DB_DB/g" /tmp/create.sql > tmp1; cat tmp1 > /tmp/create.sql; rm tmp1
 sudo service postgresql start
 sudo -u postgres psql -f /tmp/create.sql
+tail -f /dev/null
