3ff40eac53d2888a7730afc58b3b37d215b84273
cescanue <cescanuela@gmail.com>
2024-04-17 18:28:50 +0200

    django

--- infra/scrs/requirements/django/conf/entry.sh
+++ infra/scrs/requirements/django/conf/entry.sh
@@ -1,11 +1,16 @@
-sudo service ssh start 
+#!/bin/bash
+echo root:$ROOT_PASSWORD | sudo chpasswd
+service ssh start 
 echo Waiting postgress up
 sleep 5
 if [ -f "/pong/manage.py" ]
 then
-	sudo python3 manage.py runserver 0.0.0.0:8000 
+	python3 manage.py runserver 0.0.0.0:8000 
 else
-	sudo django-admin startproject pong /pong
-	sudo cp /tmp/settings.py /pong/pong/
-	sudo python3 manage.py runserver 0.0.0.0:8000 
+ 	django-admin startproject pong /pong
+	cp /tmp/settings.py /pong/pong/
+	python3 manage.py makemigrations
+	python3 manage.py migrate
+	python3 manage.py createsuperuser --no-input
+	python3 manage.py runserver 0.0.0.0:8000 
 fi
--- infra/scrs/requirements/postgresql/conf/create.sh
+++ infra/scrs/requirements/postgresql/conf/create.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+echo root:$ROOT_PASSWORD | sudo chpasswd
 if [ -f "/var/lib/postgresql/postgres.check" ]
 then
-	sudo service postgresql start
+	service postgresql start
 	tail -f /dev/null
 else
 	sed "s/DB_USER/$DB_USER/g" /tmp/create.sql > tmp1; cat tmp1 > /tmp/create.sql; rm tmp1
 	sed "s/DB_PASS/$DB_PASS/g" /tmp/create.sql > tmp1; cat tmp1 > /tmp/create.sql; rm tmp1
 	sed "s/DB_DB/$DB_DB/g" /tmp/create.sql > tmp1; cat tmp1 > /tmp/create.sql; rm tmp1
-	sudo service postgresql start
+	service postgresql start
 	sudo -u postgres psql -f /tmp/create.sql
-	sudo touch "/var/lib/postgresql/postgres.check"
+	touch "/var/lib/postgresql/postgres.check"
 	tail -f /dev/null
 fi
