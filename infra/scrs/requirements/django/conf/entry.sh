c9d4452b07aceea4dca2303c30abcc5046921dd1
cescanue <cescanuela@gmail.com>
2024-04-17 22:52:08 +0200

    postgresql + django + nginx

--- infra/scrs/requirements/django/conf/entry.sh
+++ infra/scrs/requirements/django/conf/entry.sh
@@ -5,12 +5,14 @@ echo Waiting postgress up
 sleep 5
 if [ -f "/pong/manage.py" ]
 then
-	python3 manage.py runserver 0.0.0.0:8000 
+	gunicorn --bind 0.0.0.0:8000 pong.wsgi
 else
  	django-admin startproject pong /pong
 	cp /tmp/settings.py /pong/pong/
 	python3 manage.py makemigrations
 	python3 manage.py migrate
 	python3 manage.py createsuperuser --no-input
-	python3 manage.py runserver 0.0.0.0:8000 
+	python3 manage.py collectstatic --noinput
+	gunicorn --bind 0.0.0.0:8000 pong.wsgi
+	#python3 manage.py runserver 0.0.0.0:8000 
 fi
