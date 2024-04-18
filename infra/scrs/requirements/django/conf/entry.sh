3cd1ca8ea46fa3bec2bae167430dbe89872387cc
cescanue <cescanuela@gmail.com>
2024-04-18 17:24:25 +0200

    wwww

--- infra/scrs/requirements/django/conf/entry.sh
+++ infra/scrs/requirements/django/conf/entry.sh
@@ -5,14 +5,17 @@ echo Waiting postgress up
 sleep 5
 if [ -f "/pong/manage.py" ]
 then
+	cd /pong/base
 	gunicorn --bind 0.0.0.0:8000 pong.wsgi
 else
- 	django-admin startproject pong /pong
-	cp /tmp/settings.py /pong/pong/
+ 	django-admin startproject pong /pong/base/www
+	cp /tmp/settings.py /pong/base/www/pong/
 	python3 manage.py makemigrations
 	python3 manage.py migrate
 	python3 manage.py createsuperuser --no-input
 	python3 manage.py collectstatic --noinput
+	mv /pong/base /pong/basefresh
+	git clone git@github.com:jesuserr/ft_transcendence.git /pong/base
 	gunicorn --bind 0.0.0.0:8000 pong.wsgi
 	#python3 manage.py runserver 0.0.0.0:8000 
 fi
