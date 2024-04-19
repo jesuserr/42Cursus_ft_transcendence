c4ce523f91aba61e2e3f4e58094661b46e097c2a
cescanue <cescanuela@gmail.com>
2024-04-19 22:41:22 +0200

    deploy v1

--- www/deploy/views.py
+++ www/deploy/views.py
@@ -1,5 +1,5 @@
 import os
-import subprocess
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -10,5 +10,11 @@ def index(request):
     return HttpResponse(template.render())
 
 def run(request):
-    stream = os.popen("git fetch && git reset --hard HEAD && git merge origin/$CURRENT_BRANCH")
-    return HttpResponse(stream.read)
+    os.popen("sudo mkdir /pong/basebackup")
+    os.popen("sudo mkdir /pong/basenew")
+    stream = os.popen("sudo git clone git@github.com:jesuserr/ft_transcendence.git /pong/basenew && sudo rm -r /pong/basebackup && sudo mv /pong/base /pong/basebackup && sudo mv /pong/basenew /pong/base")
+    time.sleep(2)
+    while os.path.exists("/pong/basenew"):
+        time.sleep(1)
+    os.popen("pkill gunicorn")
+    return HttpResponse("Deploy finalizado")
