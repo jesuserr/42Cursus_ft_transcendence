731908fb8e3ecb043617e87b6ef7b2829a0df0fd
cescanue <cescanuela@gmail.com>
2024-04-22 16:10:30 +0200

    deploy

--- www/deploy/views.py
+++ www/deploy/views.py
@@ -11,12 +11,14 @@ def index(request):
 
 def run(request):
     deploypass = os.environ["DEPLOY_PASSWORD"]
-    return HttpResponse(deploypass)
-    os.popen("sudo mkdir /pong/basebackup")
-    os.popen("sudo mkdir /pong/basenew")
-    stream = os.popen("sudo git clone git@github.com:jesuserr/ft_transcendence.git /pong/basenew && sudo rm -r /pong/basebackup && sudo mv /pong/base /pong/basebackup && sudo mv /pong/basenew /pong/base")
-    time.sleep(2)
-    while os.path.exists("/pong/basenew"):
-        time.sleep(1)
-    os.popen("pkill gunicorn")
-    return HttpResponse("Deploy finalizado")
+    if deploypass == "" or deploypass != request.POST.get("DEPLOY_PASSWORD"):
+        return HttpResponse("Password error")
+    else:
+        os.popen("sudo mkdir /pong/basebackup")
+        os.popen("sudo mkdir /pong/basenew")
+        stream = os.popen("sudo git clone git@github.com:jesuserr/ft_transcendence.git /pong/basenew && sudo rm -r /pong/basebackup && sudo mv /pong/base /pong/basebackup && sudo mv /pong/basenew /pong/base")
+        time.sleep(2)
+        while os.path.exists("/pong/basenew"):
+            time.sleep(1)
+        os.popen("pkill gunicorn")
+        return HttpResponse("Deploy finalizado")
