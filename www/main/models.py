d4917d2bcc4abe29e595f081348f73a5daf7be5c
cescanue <cescanuela@gmail.com>
2024-04-23 11:06:12 +0200

    models

--- www/main/models.py
+++ www/main/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(verbose_name = "Email", help_text = "Your email", primary_key=True)
+    password = models.CharField(max_length=50, verbose_name = "Password")
+    displayname = models.CharField(max_length=20, verbose_name = "Display Name")
+    avatar = models.ImageField(verbose_name= "Avatar", blank=True)
+    
+    
