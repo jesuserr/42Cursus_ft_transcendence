24344b3ccd729abc0ce4e0f474a1e54847062a06
cescanue <cescanuela@gmail.com>
2024-05-03 23:16:10 +0200

    Extended ARM Config

---
+++ infra/scrs/requirements/django/conf/armconfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Lee la arquitectura del sistema
+ARCHITECTURE=$(uname -m)
+
+# Comprueba si la arquitectura es arm
+if [ "$ARCHITECTURE" = "armv7l" ]; then
+	apt install openssl libssl-dev pkg-config curl libffi-dev -y
+	curl https://sh.rustup.rs -sSf  > cargo1.sh
+	chmod 777 cargo1.sh
+	./cargo1.sh -s -- --default-toolchain=1.72.1 -y
+	export PATH="/root/.cargo/bin:${PATH}"
+    export RUSTUP_TOOLCHAIN=1.72.1
+	export OPENSSL_DIR=/usr/lib/ssl
+	mkdir /usr/lib/ssl/include
+	cp /usr/include/openssl/* /usr/lib/ssl/include
+fi
--- infra/scrs/requirements/django/conf/cargo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/dash
-curl https://sh.rustup.rs -sSf  > cargo1.sh
-chmod 777 cargo1.sh
-./cargo1.sh -y
