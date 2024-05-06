d6e65ec20f91f0257eb4395d259ab53815467df2
cescanue <cescanuela@gmail.com>
2024-05-06 10:40:52 +0200

    ARM fine tunning3

--- infra/scrs/requirements/django/conf/armconfig.sh
+++ infra/scrs/requirements/django/conf/armconfig.sh
@@ -6,9 +6,9 @@ ARCHITECTURE=$(uname -m)
 # Comprueba si la arquitectura es arm
 if [ "$ARCHITECTURE" = "armv7l" ]; then
 	apt install openssl libssl-dev pkg-config curl libffi-dev -y
-	curl https://sh.rustup.rs -sSf  > cargo1.sh
-	chmod 777 cargo1.sh
-	./cargo1.sh --default-toolchain=1.72.1 -y
+	curl https://sh.rustup.rs -sSf  > /tmp/cargo1.sh
+	chmod 777 /tmp/cargo1.sh
+	/tmp/cargo1.sh --default-toolchain=1.72.1 -y
 	export PATH="/root/.cargo/bin:${PATH}"
     export RUSTUP_TOOLCHAIN=1.72.1
 	export OPENSSL_DIR=/usr/lib/ssl
