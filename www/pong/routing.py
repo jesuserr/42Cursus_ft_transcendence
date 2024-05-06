ed5479ebe18820a3600744053eb2057dab444ef6
cescanue <cescanuela@gmail.com>
2024-05-06 12:55:40 +0200

    gameroot2

--- www/pong/routing.py
+++ www/pong/routing.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
 from chat import consumers
+from game import gameconsumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-	re_path(r"ws/game/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+	re_path(r"ws/game/(?P<game_name>\w+)/$", gameconsumers.ChatConsumer.as_asgi()),
 ]
