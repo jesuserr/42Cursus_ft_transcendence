358523ca95520f291e9acd507a4d48fbacfae93e
cescanue <cescanuela@gmail.com>
2024-05-06 09:52:00 +0200

    ARM fine tunning1

--- infra/scrs/requirements/django/conf/armconfig.sh
+++ infra/scrs/requirements/django/conf/armconfig.sh
@@ -9,9 +9,9 @@ if [ "$ARCHITECTURE" = "armv7l" ]; then
 	curl https://sh.rustup.rs -sSf  > cargo1.sh
 	chmod 777 cargo1.sh
 	./cargo1.sh --default-toolchain=1.72.1 -y
+	export PATH="/root/.cargo/bin:${PATH}"
     export RUSTUP_TOOLCHAIN=1.72.1
 	export OPENSSL_DIR=/usr/lib/ssl
-	export PATH="/root/.cargo/bin:${PATH}"
 	mkdir /usr/lib/ssl/include
 	cp /usr/include/openssl/* /usr/lib/ssl/include
 fi
