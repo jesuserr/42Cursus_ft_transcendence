1f94a0c6a8f74514ed6f7bc0e9b320d5aad2930d
cescanue <cescanuela@gmail.com>
2024-05-08 22:10:51 +0200

    chat cleaning code

--- www/chat/chatconsumers.py
+++ www/chat/chatconsumers.py
@@ -1,66 +1,67 @@
 import json
-import asyncio
-import functools
-import time
-from datetime import datetime
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from main.models import User
 from channels.db import database_sync_to_async
 from django.core import serializers
-import django.core.serializers
-from django.forms.models import model_to_dict
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    #When the connection is established
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]
         self.room_group_name = f"chat"
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         #Check user is logged
         if 'sessionid' in self.scope['cookies'].keys():
-          await self.getUser()
-          if bool(self.user):
-               await self.accept()
-               await self.send(text_data=json.dumps({"User": str(self.user.displayname)}))
+            await self.getUser()
+            if bool(self.user):
+                await self.accept()
+                #send to client some information
+                await self.send_json({"User": str(self.user.displayname)})
+                await self.sendUserList()
 
+    #When the connection is closed              
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-
-    async def receive(self, text_data):
-        data = json.loads(text_data)
-        if 'list_users' in text_data:
-              await self.sendUserList()
-        elif 'chat_selected_user' in text_data:
-              print(self.user.displayname + ":" + text_data)
-		#text_data_json = json.loads(text_data)
+    
+    #When receive a message
+    async def receive_json(self, data):
+        #check the command
+        if 'list_users' in data:
+            await self.sendUserList()
+        elif 'chat_selected_user' in data:
+            print(self.user.displayname + ":" + data)
+    
+        #text_data_json = json.loads(text_data)
         #message = text_data_json["message"]
         #await self.channel_layer.group_send(
         #    self.room_group_name, {"type": "chat.message", "message": self.user.email}
         #)
-    
-    async def sendUserList(self):
-        data = await self.getUserList()
-        print(data)
-        await self.send(text_data=data)
-
+    #When receive a message from the group
     async def chat_message(self, event):
         message = event["message"]
         await self.send(text_data=json.dumps({"message": message}))
-        
+    
+    #Send the user list to the client
+    async def sendUserList(self):
+        data = await self.getUserList()
+        await self.send_json(data)        
+    
+    #Get the user list
     @database_sync_to_async
     def getUserList(self):
-          print("getUserList")
-          data = serializers.serialize('json', User.objects.all(), fields=('displayname'))
-          data_obj = json.loads(data)
-          new_obj = {'list_users': data_obj,}
-          return json.dumps(new_obj)
-     
+        data = serializers.serialize('json', User.objects.all(), fields=('displayname'))
+        data_obj = json.loads(data)
+        new_obj = {'list_users': data_obj,}
+        return new_obj
+    
+    #Get the user
     @database_sync_to_async
     def getUser(self):
-    	try:
-        		self.user = User.objects.get(sessionid=self.scope['cookies']['sessionid'])
-    	except:
-                self.user = ""
+        try:
+            self.user = User.objects.get(sessionid=self.scope['cookies']['sessionid'])
+        except:
+            self.user = ""
     
          
         
--- www/static/chat/chat.js
+++ www/static/chat/chat.js
@@ -1,35 +1,43 @@
+//Connect the websocket to the server
 const socket = new WebSocket(
 	'wss://'
 	+ window.location.host
 	+ '/ws/chat/'
 );
 
+// When the socket is open, display the connection status
 if (socket.readyState != WebSocket.CLOSED) {
 	document.getElementById("status").innerText = "Connected";
 	document.getElementById("connection_status").src = "/static/chat/connected.png";
 };
-
- socket.onclose = function (e) {
+// When the socket is closed, display the connection status
+socket.onclose = function (e) {
 	document.getElementById("status").innerText = "Disconnected";
 	document.getElementById("connection_status").src = "/static/chat/disconnect.png";
  };
-
- socket.onmessage = function (e) {
+//when socket reveives a message, decode the command and run the appropriate function
+socket.onmessage = function (e) {
+	//parse the data
 	const data = JSON.parse(e.data);
-	//esto rellena el select
-	for (let i = 0; i < data.list_users.length; i++) {
-		const opt = document.createElement("option");
-		opt.value = data.list_users[i].pk;
-		opt.text = data.list_users[i].fields.displayname;
-		document.getElementById("userlist").add(opt, null)
-	};
-
-
+	//To deleted, show the data in the console
+	console.log(data);
+	//check the command and run the appropriate function
+	//if the command is "User", display the user's displayname
 	if (data.hasOwnProperty("User")) {
 		document.getElementById("displayname").innerText = data.User;
+	}
+	//if the command is "list_users", display the list of users
+	else if (data.hasOwnProperty("list_users")) {
+		for (let i = 0; i < data.list_users.length; i++) {
+			const opt = document.createElement("option");
+			opt.value = data.list_users[i].pk;
+			opt.text = data.list_users[i].fields.displayname;
+			document.getElementById("userlist").add(opt, null)
+		};
 	};
 };
 
+// When the user clicks the send button, send the command to server
 document.querySelector('#chat_all_users').onclick = function (e) {
 		socket.send(JSON.stringify({'list_users': 'list_all_users'}));
 	};
