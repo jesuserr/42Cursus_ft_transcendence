dbeece7577af02a4a2a1012d425e199060160dc5
cescanue <cescanuela@gmail.com>
2024-06-18 16:34:23 +0200

    tournament mode

--- www/tournament/models.py
+++ www/tournament/models.py
@@ -7,4 +7,4 @@ class Tournament_List(models.Model):
 
 class Tournament_Connected_Users(models.Model):
     tournamnet_name = models.ForeignKey(Tournament_List, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
