e36112821cfda6ed761a55f6257bc9461b39000d
cescanue <cescanuela@gmail.com>
2024-07-25 22:17:50 +0200

    Change git clone for copy

--- requirements/django/conf/entry.sh
+++ requirements/django/conf/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo root:$ROOT_PASSWORD | sudo chpasswd
-service ssh start 
+service ssh start
 if [ -f "/pong/base/www/manage.py" ]
 then
 	python3 manage.py makemigrations --noinput
@@ -10,15 +10,15 @@ then
 	#gunicorn --bind 0.0.0.0:8000 pong.wsgi pong.asgi --reload 
 else
 	echo Waiting postgresql .....
-	#sleep 4
+	sleep 4
  	#django-admin startproject pong /pong/base/www
 	#cp /tmp/settings.py /pong/base/www/pong/
 	#python3 manage.py makemigrations --noinput
 	#python3 manage.py migrate --noinput
 	#python3 manage.py createsuperuser --no-input
 	#python3 manage.py collectstatic --noinput
-	mv /pong/base /pong/basefresh
-	git clone git@github.com:jesuserr/ft_transcendence.git /pong/base
+	#mv /pong/base /pong/basefresh
+	#git clone git@github.com:jesuserr/ft_transcendence.git /pong/base
 	#python3 manage.py makemigrations
 	#python3 manage.py migrate
 	#gunicorn --bind 0.0.0.0:8000 pong.wsgi --reload 
